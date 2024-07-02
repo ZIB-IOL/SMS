63c2ad5269303787f748aff89213774b96a77145
zimmer-m <zimmer@zib.de>
2024-07-02 08:55:56 +0200

    Cleaned up main.py, added comments

--- main.py
+++ main.py
@@ -24,42 +24,42 @@ from utilities.utilities import Utilities as Utils
 debug = "--debug" in sys.argv
 defaults = dict(
         # System
-        run_id=1,
-        computer=socket.gethostname(),
+        run_id=1,                                                   # The run id, determines the original random seed
+        computer=socket.gethostname(),                              # The computer that runs the experiment
 
         # Setup
-        dataset='mnist',
-        arch='Simple',
-        n_epochs=2,
-        batch_size=1028,
+        dataset='mnist',                                            # The dataset to use, see config.py for available options
+        arch='Simple',                                              # The architecture to use, see models/ for available options
+        n_epochs=2,                                                 # The number of epochs to pretrain the model for (Note: this only controls the pretraining)
+        batch_size=1028,                                            # The batch size to use
 
         # Efficiency
-        use_amp=True,
+        use_amp=True,                                               # Whether to use automatic mixed precision
 
         # Optimizer
-        optimizer='SGD',
-        learning_rate='(Linear, 0.1)',
-        n_epochs_warmup=None,  # number of epochs to warmup the lr, should be an int
-        momentum=0.9,
-        weight_decay=0.0001,
+        optimizer='SGD',                                            # The optimizer to use for pretraining/retraining, currently only SGD implemented
+        learning_rate='(Linear, 0.1)',                              # The learning rate to use for pretraining
+        n_epochs_warmup=None,                                       # The number of epochs to warmup the lr, must be an int
+        momentum=0.9,                                               # The momentum to use for the optimizer
+        weight_decay=0.0001,                                        # The weight decay to use for the optimizer
 
         # Sparsifying strategy
-        strategy='Dense',
-        goal_sparsity=0.9,
-        pruning_selector='global',  # must be in ['global', 'uniform', 'random', 'LAMP']
+        strategy='Dense',                                           # The strategy to use, see strategies/ for available options. 'Dense' = pretraining, 'IMP' = iterative magnitude pruning, 'Ensemble' = ensembl/soup methods
+        goal_sparsity=0.9,                                          # The goal sparsity to reach after n_phases many prune-retrain cycles
+        pruning_selector='global',                                  # Pruning allocation, must be in ['global', 'uniform', 'random']
 
         # Retraining
-        phase=1,
-        n_phases=1,
-        n_epochs_per_phase=1,
-        retrain_schedule='LLR',
+        phase=1,                                                    # The current phase of IMP/Ensemble
+        n_phases=1,                                                 # The total number of phases to run
+        n_epochs_per_phase=1,                                       # The number of epochs to retrain for each phase
+        retrain_schedule='LLR',                                     # The retrain lr schedule, must be in ['FT', 'LRW', 'SLR', 'CLR', 'LLR', 'ALLR']
 
         # Ensemble method
-        ensemble_method='UniformEnsembling',
-        ensemble_by='pruned_seed',  # ['pruned_seed', 'weight_decay', 'retrain_length', 'retrain_schedule']
-        split_val=None,
-        n_splits_total=2,  # The total number of splits we expect to have -> will raise an error if not that number
-        bn_recalibration_frac=0.2,
+        ensemble_method='UniformEnsembling',                        # The ensemble/soup method to use, must be in ['UniformEnsembling', 'GreedySoup']
+        ensemble_by='pruned_seed',                                  # The parameter controlling what is varied during retraining, must be in ['pruned_seed', 'weight_decay', 'retrain_length', 'retrain_schedule']
+        split_val=None,                                             # The value to split the ensemble_by parameter on, e.g. ensemble_by='weight_decay' and split_val=0.0001 will retrain with a weight decay of 0.0001
+        n_splits_total=2,                                           # The total number of splits we expect to have, will raise an error if more models to average found
+        bn_recalibration_frac=0.2,                                  # The fraction of the dataset to use for recalibrating the batch norm layers, must be in [0,1]
     )
 
 if not debug:
